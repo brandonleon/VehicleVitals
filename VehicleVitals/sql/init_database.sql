3cea297bd43a0704ba84bf2ab5831f55119e2e76
Brandon Leon <brandon.leon@servicenow.com>
2023-11-24 14:07:26 -0800

    feat: Add version and configurations tables to database schema
    This commit introduces changes to the database schema by adding two new tables:
    - `version`: Represents the version of the database schema with an auto-incremented ID and a schema version number.
    - `configurations`: Stores key-value pairs for various application configurations.
    
    These additions enhance the database structure, providing a mechanism to track the schema version and manage application configurations. Additionally, the version is updated to 0.3.0 in the `pyproject.toml` file.

--- VehicleVitals/sql/init_database.sql
+++ VehicleVitals/sql/init_database.sql
@@ -1,3 +1,17 @@
+-- Version table
+CREATE TABLE IF NOT EXISTS "version" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "schema_version" INTEGER NOT NULL
+);
+
+-- configurations table
+CREATE TABLE IF NOT EXISTS "configurations" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "key" TEXT NOT NULL,
+    "value" TEXT,
+    UNIQUE ("key")
+);
+
 -- logs table
 CREATE TABLE IF NOT EXISTS "logs" (
     ID              TEXT,
