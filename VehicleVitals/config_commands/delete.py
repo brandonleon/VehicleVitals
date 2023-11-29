ff1862859fdceddc5f5f63cbc504a67c6a7ecb36
Brandon Leon <brandon.leon@servicenow.com>
2023-11-29 12:50:35 -0800

    ℹ️ Enhance delete functionality with warnings and confirmation
    This commit introduces improvements to the delete functionality in the `VehicleVitals` application. Specifically, it adds informative warnings before deletion to alert the user about potential consequences. When attempting to delete a fuel type, service type, or part, a warning message is displayed using the `rich` library, emphasizing that the action will also delete any associated service records.
    
    The commit includes the following changes:
    - Added rich library import for enhanced console output.
    - Incorporated warning messages before confirming the deletion of fuel types, service types, and parts.
    - Updated docstrings for the delete functions to provide clear information about their purpose.
    
    These changes aim to enhance user awareness and prevent accidental deletions of crucial data.

--- VehicleVitals/config_commands/delete.py
+++ VehicleVitals/config_commands/delete.py
@@ -5,6 +5,7 @@ import sqlite3
 from typing import Annotated
 from uuid import uuid4
 
+import rich
 import typer
 from rich.console import Console
 from rich.table import Table
@@ -19,7 +20,13 @@ def fuel_type(
     fuel: Annotated[str, typer.Option(help="Name or ID of the fuel type to delete")],
     confirm: Annotated[bool, typer.Option(help="Confirm the deletion")] = True,
 ):
+    """
+    Delete a fuel type from the database.
+    """
     if confirm:
+        rich.print(
+            "[bold red]WARNING![/bold red] Deleting a fuel type will also delete any associated service records."
+        )
         typer.confirm("Are you sure you want to delete this fuel type?", abort=True)
 
     with sqlite3.connect(get_db_location()) as conn:
@@ -39,7 +46,13 @@ def service_type(
     ],
     confirm: Annotated[bool, typer.Option(help="Confirm the deletion")] = True,
 ):
+    """
+    Delete a service type from the database.
+    """
     if confirm:
+        rich.print(
+            "[bold red]WARNING![/bold red] Deleting a service type will also delete any associated service records."
+        )
         typer.confirm("Are you sure you want to delete this service type?", abort=True)
 
     with sqlite3.connect(get_db_location()) as conn:
@@ -57,7 +70,13 @@ def part(
     part: Annotated[str, typer.Option(help="Name or ID of the part to delete")],
     confirm: Annotated[bool, typer.Option(help="Confirm the deletion")] = True,
 ):
+    """
+    Delete a part from the database.
+    """
     if confirm:
+        rich.print(
+            "[bold red]WARNING![/bold red] Deleting a part will also delete any associated service records."
+        )
         typer.confirm("Are you sure you want to delete this part?", abort=True)
 
     with sqlite3.connect(get_db_location()) as conn:
